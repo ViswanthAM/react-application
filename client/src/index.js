f05ebb97ddd5fdce0776cb42c77e7c65745c4815
ViswanthAM <viswantham@gmail.com>
2019-02-23 22:03:48 +0530

    Basic structure of the application

---
+++ client/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware, compose} from 'redux'
+import ReduxThunk from 'redux-thunk'
+
+import App from './components/App';
+import reducers from './reducer';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers,composeEnhancers( applyMiddleware( ReduxThunk ) ));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,document.querySelector('#root'));
